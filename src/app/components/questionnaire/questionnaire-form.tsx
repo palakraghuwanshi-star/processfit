2dc435f1de3d005f6e65b22359a85cab05564ad7
Palak Raghuwanshi <palak.raghuwanshi@zamp.ai>
2025-12-10 06:58:46 +0000

    Debug this issue and tell me what the problem is?

--- src/app/components/questionnaire/questionnaire-form.tsx
+++ src/app/components/questionnaire/questionnaire-form.tsx
@@ -567,7 +567,7 @@ const Section7 = () => (
                 <FormItem>
                     <FormLabel>Is this manual process limiting your ability to scale or grow?</FormLabel>
                     <FormControl>
-                        <RadioGroup onValue-change={field.onChange} defaultValue={field.value} className="space-y-2 pt-1">
+                        <RadioGroup onValueChange={field.onChange} defaultValue={field.value} className="space-y-2 pt-1">
                             {Options.growthLimitOptions.map(o => (
                                 <FormItem key={o} className="flex items-center space-x-3 space-y-0">
                                     <FormControl><RadioGroupItem value={o} /></FormControl>

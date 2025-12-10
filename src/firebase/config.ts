ce896f50997c6187ade413f46dcee0aebb55fcce
Palak Raghuwanshi <palak.raghuwanshi@zamp.ai>
2025-12-10 07:14:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9760069725-f84f3",
+  "appId": "1:854359913186:web:9e88a0e7061e497d2aa4bb",
+  "apiKey": "AIzaSyCAJ5qJ5UwqETp6H69ihi78xXoRe0-rrXI",
+  "authDomain": "studio-9760069725-f84f3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "854359913186"
+};

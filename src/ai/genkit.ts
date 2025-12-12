48eab0967b093bfc07463ddae1e14f84115e41cf
Palak Raghuwanshi <palak.raghuwanshi@zamp.ai>
2025-12-12 14:54:49 +0000

    updated

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+      apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
